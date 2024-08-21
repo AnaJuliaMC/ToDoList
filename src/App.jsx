6dd23d6797abce1106f1b9093a9fd415bfa68e0c
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-08-21 11:46:36 -0300

    componente e snippets

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+
 export default function App(){
 
   return (
     <>
-      <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At vero minus deleniti pariatur provident molestiae autem quaerat incidunt sunt, quis blanditiis sequi soluta porro? Esse blanditiis qui natus? Deleniti, soluta?</h1> 
+      <Header />
+      <Main />
+      <Footer />
     </>
+   
   )
 }
