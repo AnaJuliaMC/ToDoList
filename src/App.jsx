0a2d364608ab18055da65f71a02f9ad40fa53e2b
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-08-21 11:00:12 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App(){
+
+  return (
+    <>
+      <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At vero minus deleniti pariatur provident molestiae autem quaerat incidunt sunt, quis blanditiis sequi soluta porro? Esse blanditiis qui natus? Deleniti, soluta?</h1> 
+    </>
+  )
+}
