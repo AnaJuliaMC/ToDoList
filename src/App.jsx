1ff49332217b067ca8236424ce32c7f8c5b2c54d
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-08-30 09:51:30 -0300

    comeco do jeito do professor

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
 import Footer from "./components/Footer/Footer"
 
+import styles from "./App.module.css"
 
 export default function App(){
 
   return (
-    <>
+    <div className={styles.App}>
       <Header />
 
       <Main>
+        <form action="">
         <input type="text" />
-        <button></button>
+        <button></button>          
+        </form>
+        {/* <TaskList/> */}
       </Main>
       <Footer autor="Ana"/>
-    </>
+      </div>
    
   )
 }
